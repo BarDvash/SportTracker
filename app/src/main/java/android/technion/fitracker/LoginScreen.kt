1e5e40caa68f492263c162e6f62df39a1633b4e1
Vladimir Parakhin <vov4ikpa@gmail.com>
2019-12-02 01:23:34 +0200

    Created Login screen Activity

--- app/src/main/java/android/technion/fitracker/LoginScreen.kt
+++ app/src/main/java/android/technion/fitracker/LoginScreen.kt
@@ -3,10 +3,10 @@ package android.technion.fitracker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login_screen)
     }
 }
