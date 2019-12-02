b4ba2adf6fd8e9036081dc1b1968277cd46dd708
Vladimir Parakhin <vov4ikpa@gmail.com>
2019-12-03 01:41:14 +0200

    Added navigation paths to sign up as regular user fragment

--- app/src/main/java/android/technion/fitracker/SignUpFragment.kt
+++ app/src/main/java/android/technion/fitracker/SignUpFragment.kt
@@ -6,11 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 /**
  * A simple [Fragment] subclass.
  */
-class SignUpFragment : Fragment() {
+class SignUpFragment : Fragment(), View.OnClickListener {
+
+    lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +23,20 @@ class SignUpFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.sign_up_fragment_sign_up_button).setOnClickListener(this)
+        view.findViewById<Button>(R.id.sign_up_fragment_trainer_sign_up_button).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.sign_up_fragment_sign_up_button -> navController.navigate(R.id.action_signUpFragment_to_signInFragment);
+            R.id.sign_up_fragment_trainer_sign_up_button -> navController.navigate(R.id.action_signUpFragment_to_signUpBusinessFragment);
+        }
     }
 
 
