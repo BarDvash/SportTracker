112707c15a176621382a357a303dfe7350dc476d
Vladimir Parakhin <vov4ikpa@gmail.com>
2019-12-02 02:12:34 +0200

    Created buisiness sign up activity

---
+++ app/src/main/java/android/technion/fitracker/SignUpBuisiness.kt
@@ -0,0 +1,12 @@
+package android.technion.fitracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpBuisiness : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_buisiness)
+    }
+}
