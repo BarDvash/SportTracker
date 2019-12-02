7f64933abc14b0376560de217043ceb212f13405
Vladimir Parakhin <vov4ikpa@gmail.com>
2019-12-03 01:23:43 +0200

    Added navigation links to sign in fragment, refactored naming

--- app/src/main/java/android/technion/fitracker/SignInFragment.kt
+++ app/src/main/java/android/technion/fitracker/SignInFragment.kt
@@ -2,15 +2,20 @@ package android.technion.fitracker
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 /**
  * A simple [Fragment] subclass.
  */
-class SignInFragment : Fragment() {
+class SignInFragment : Fragment(), View.OnClickListener {
+
+    lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +23,24 @@ class SignInFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sign_in, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.login_fragment_sign_in_button).setOnClickListener(this)
+        view.findViewById<Button>(R.id.login_fragment_sign_in_with_google).setOnClickListener(this)
+        view.findViewById<Button>(R.id.login_fragment_sign_up_button).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        // TODO("We should manage here the logic for the trainer/user login")
+        when (v!!.id) {
+            R.id.login_fragment_sign_in_button -> navController.navigate(R.id.action_signInFragment_self);
+            R.id.login_fragment_sign_in_with_google -> navController.navigate(R.id.action_signInFragment_self);
+            R.id.login_fragment_sign_up_button -> navController.navigate(R.id.action_signInFragment_to_signUpFragment);
+
+        }
     }
 
 
