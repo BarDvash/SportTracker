aeb9b57890d266fcbd832af92dbc8cf14b2ae0ef
Vladimir-pa <vov4ikpa@gmail.com>
2019-12-03 11:34:34 +0200

    Created User's activity

---
+++ app/src/main/java/android/technion/fitracker/user/UserActivity.kt
@@ -0,0 +1,13 @@
+package android.technion.fitracker.user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.technion.fitracker.R
+
+class UserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user)
+    }
+}
