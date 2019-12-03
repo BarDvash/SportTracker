2ff6323f6d628c2e35d8d4ac9cda3c9c8aaf85e2
Vladimir-pa <vov4ikpa@gmail.com>
2019-12-03 23:00:32 +0200

    Configured constraints in home screen

--- app/src/main/java/android/technion/fitracker/user/UserActivity.kt
+++ app/src/main/java/android/technion/fitracker/user/UserActivity.kt
@@ -9,5 +9,6 @@ class UserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
     }
 }
