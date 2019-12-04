e42e4e902e3f47e8e2359f2df83333507566c120
axel92b <axel92b@gmail.com>
2019-12-04 21:29:18 +0200

    Implemented sign in and user sign up features, closes #22, closes #3

--- app/src/main/java/android/technion/fitracker/login/SignUpFragment.kt
+++ app/src/main/java/android/technion/fitracker/login/SignUpFragment.kt
@@ -1,22 +1,47 @@
 package android.technion.fitracker.login
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.technion.fitracker.R
+import android.technion.fitracker.user.UserActivity
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import com.google.firebase.auth.FirebaseAuth
 
 /**
  * A simple [Fragment] subclass.
  */
 class SignUpFragment : Fragment(), View.OnClickListener {
 
-    lateinit var navController: NavController
+    private lateinit var navController: NavController
+    private lateinit var auth: FirebaseAuth
+
+    //Fields
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            startUserActivity()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,18 +56,38 @@ class SignUpFragment : Fragment(), View.OnClickListener {
         navController = Navigation.findNavController(view)
         view.findViewById<Button>(R.id.sign_up_fragment_sign_up_button).setOnClickListener(this)
         view.findViewById<Button>(R.id.sign_up_fragment_trainer_sign_up_button).setOnClickListener(this)
+
+        emailEditText = view.findViewById(R.id.sign_up_fragment_email)
+        passwordEditText = view.findViewById(R.id.sign_up_fragment_pass)
     }
 
     override fun onClick(v: View?) {
         when(v!!.id){
-            R.id.sign_up_fragment_sign_up_button -> navController.navigate(
-                R.id.action_signUpFragment_to_signInFragment
-            );
+            R.id.sign_up_fragment_sign_up_button -> handleEmailSignUp()
             R.id.sign_up_fragment_trainer_sign_up_button -> navController.navigate(
                 R.id.action_signUpFragment_to_signUpBusinessFragment
-            );
+            )
         }
     }
 
+    private fun handleEmailSignUp() {
+        val email = emailEditText.text.toString()
+        val password = passwordEditText.text.toString()
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(activity!!) { task ->
+                if (task.isSuccessful) {
+                    // Sign up success
+                    startUserActivity()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(context, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
 
+    private fun startUserActivity() {
+        val userHome = Intent(context!!, UserActivity::class.java)
+        startActivity(userHome)
+        activity?.finish()
+    }
 }
