96dcb6cdfbf842c8490698edd5169ca3b2b247e7
Vladimir Parakhin <vov4ikpa@gmail.com>
2019-12-04 23:34:20 +0200

    Added template of user avatar and user name

--- app/src/main/java/android/technion/fitracker/user/UserActivity.kt
+++ app/src/main/java/android/technion/fitracker/user/UserActivity.kt
@@ -5,6 +5,7 @@ import android.technion.fitracker.R
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.google.android.material.bottomnavigation.BottomNavigationView
