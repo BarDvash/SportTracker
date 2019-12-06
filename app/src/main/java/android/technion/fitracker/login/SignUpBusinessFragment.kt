11cac01e5668b49fb8d24eee9330e09de807ca3b
axel92b <axel92b@gmail.com>
2019-12-06 18:24:46 +0200

    Implemented trainer sign up

--- app/src/main/java/android/technion/fitracker/login/SignUpBusinessFragment.kt
+++ app/src/main/java/android/technion/fitracker/login/SignUpBusinessFragment.kt
@@ -1,15 +1,20 @@
 package android.technion.fitracker.login
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.technion.fitracker.R
+import android.technion.fitracker.user.business.BusinessUserActivity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import com.google.firebase.auth.FirebaseAuth
 
 /**
  * A simple [Fragment] subclass.
@@ -17,6 +22,25 @@ import androidx.navigation.Navigation
 class SignUpBusinessFragment : Fragment(), View.OnClickListener {
 
     lateinit var navController: NavController
+    private lateinit var auth: FirebaseAuth
+
+    //Fields
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            startBusinessUserActivity()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,18 +55,40 @@ class SignUpBusinessFragment : Fragment(), View.OnClickListener {
         navController = Navigation.findNavController(view)
         view.findViewById<Button>(R.id.sign_up_business_sign_up_button).setOnClickListener(this)
         view.findViewById<Button>(R.id.sign_up_business_regular_user_button).setOnClickListener(this)
+
+        emailEditText = view.findViewById(R.id.sign_up_business_email)
+        passwordEditText = view.findViewById(R.id.sign_up_business_pass)
     }
 
     override fun onClick(v: View?) {
         when(v!!.id){
-            R.id.sign_up_business_sign_up_button -> navController.navigate(
-                R.id.action_signUpBusinessFragment_to_signInFragment
-            );
+            R.id.sign_up_business_sign_up_button -> handleEmailSignUp()
             R.id.sign_up_business_regular_user_button -> navController.navigate(
                 R.id.action_signUpBusinessFragment_to_signUpFragment
-            );
+            )
         }
     }
 
+    private fun handleEmailSignUp() {
+        val email = emailEditText.text.toString()
+        val password = passwordEditText.text.toString()
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(activity!!) { task ->
+                if (task.isSuccessful) {
+                    //TODO add to DB that user is business user
+                    // Sign up success
+                    startBusinessUserActivity()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(context, getString(R.string.authentication_failed), Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun startBusinessUserActivity() {
+        val userHome = Intent(context!!, BusinessUserActivity::class.java)
+        startActivity(userHome)
+        activity?.finish()
+    }
 
 }
