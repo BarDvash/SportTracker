4ddad1920c7e0d8a2a0b7051e2e44fb39330560f
Vladimir Parakhin <vov4ikpa@gmail.com>
2019-12-07 16:07:33 +0200

    Added firestore connection to display workouts for user

---
+++ app/src/main/java/android/technion/fitracker/adapters/WorkoutsFireStoreAdapter.kt
@@ -0,0 +1,40 @@
+package android.technion.fitracker.adapters
+
+import android.technion.fitracker.R
+import android.technion.fitracker.adapters.WorkoutsFireStoreAdapter.ViewHolder
+import android.technion.fitracker.models.WorkoutFireStoreModel
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+
+class WorkoutsFireStoreAdapter(options: FirestoreRecyclerOptions<WorkoutFireStoreModel>) :
+    FirestoreRecyclerAdapter<WorkoutFireStoreModel, ViewHolder>(options) {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.workout_ele, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(
+        holder: ViewHolder,
+        position: Int,
+        item: WorkoutFireStoreModel
+    ) {
+        holder.name.text = item.name
+        holder.desc.text = item.desc
+    }
+
+    inner class ViewHolder(view: View) :
+        RecyclerView.ViewHolder(view){
+        var name: TextView = view.findViewById(R.id.workoutName)
+        var desc: TextView = view.findViewById(R.id.workoutInfo)
+    }
+}
