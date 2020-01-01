751e1573f5d5b9ca0a52ae6e403d02208094f62e
Vladimir-pa <vov4ikpa@gmail.com>
2020-01-01 22:47:09 +0200

    Removed unnecessary field from firestore

--- app/src/main/java/com/technion/fitracker/user/User.kt
+++ app/src/main/java/com/technion/fitracker/user/User.kt
@@ -1,7 +1,6 @@
 package com.technion.fitracker.user
 
 data class User(
-    val type: String? = null,
     val name: String? = null,
     val phone: String? = null,
     val photoURL: String? = null
